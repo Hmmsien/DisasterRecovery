ff31cb62ade62d9173c23092da683442dba089e4
Huimin Chen <98779692+Hmmsien@users.noreply.github.com>
2023-10-04 23:39:38 -0400

    User Identity

---
+++ DisasterRecovery/Pages/JobCodeManagement/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace DisasterRecovery.Pages.JobCodeManagement
+{
+	public class IndexModel : PageModel
+    {
+        public IEnumerable<JobCode> JobCodes { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            using(var httpClient = new HttpClient())
+            {
+                using(var response = await httpClient.GetAsync("http://localhost:5113/api/JobCode/Get"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    JobCodes = JsonConvert.DeserializeObject<IEnumerable<JobCode>>(apiResponse);
+                }
+            };
+
+            return Page();
+        }
+    }
+}
---
+++ WebAPI/Controllers/JobCodesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DataAccessLayer.Entity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Repository.JobCodeManagement;
+
+namespace WebAPI.Controllers
+{
+	public class JobCodesController : ControllerBase
+	{
+		private readonly IJobCodeRepository _jobCodeRepository;
+
+		public JobCodesController(IJobCodeRepository repository)
+		{
+			_jobCodeRepository = repository;
+		}
+
+		[HttpGet]
+		[Route("api/JobCode/Get")]
+		public async Task<IEnumerable<JobCode>> Get()
+		{
+			return await _jobCodeRepository.GetAllJobCodes();
+		}
+
+		[HttpPost]
+		[Route("api/JobCode/Create")]
+		public async Task CreateAsync([FromBody] JobCode jobCode)
+		{
+			if (ModelState.IsValid)
+			{
+				await _jobCodeRepository.AddJobCode(jobCode);
+			}
+		}
+	}
+}
+
