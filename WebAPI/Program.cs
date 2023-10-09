fcdb87ed6ab0593ccc52774111bdac09cc06db7d
Gabriel Adelemoni <gabadelemoni@outlook.com>
2023-10-09 00:44:49 -0400

    Final Project Implementation

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,10 @@
-using DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Repository.MachineCodeManagement;
 using WebAPI.Repository.MachineManagement;
+using WebAPI.Repository.JobCodeManagement;
+using WebAPI.Repository.TimesheetManagement;
+using Microsoft.AspNetCore.Identity;
+using DataAccessLayer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +14,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IJobCodeRepository, JobCodeRepository>();
 builder.Services.AddTransient<IMachineCodeRepository, MachineCodeRepository>();
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddTransient<ITimesheetRepository, TimesheetRepository>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages(); ;
 
 var app = builder.Build();
 
